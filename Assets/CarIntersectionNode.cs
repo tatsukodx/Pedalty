a01b5db2a655a55a71d75cd72cccde788d81a921
tatsukodx <tatsuhitodx0924@gmail.com>
2026-08-17 13:50:09 +0900

    change map and script

--- Assets/CarIntersectionNode.cs
+++ Assets/CarIntersectionNode.cs
@@ -3,46 +3,51 @@ using System.Collections;
 
 public class CarIntersectionNode : MonoBehaviour
 {
-    public float straightDelay = 0.5f;
-    public float leftTurnDelay = 0.5f;
-    public float rightTurnDelay = 1f;
+    public float straightDistance = 6f;
+    public float leftTurnDistance = 6f;
+    public float rightTurnDistance = 10f;
 
     private void OnTriggerEnter(Collider other)
     {
         CarController car = other.GetComponent<CarController>();
         if (car != null)
         {
-            StartCoroutine(TurnWithDelay(car, other.transform));
+            StartCoroutine(TurnAfterDistance(car, other.transform));
         }
     }
 
-    private IEnumerator TurnWithDelay(CarController car, Transform carTransform)
+    private IEnumerator TurnAfterDistance(CarController car, Transform carTransform)
     {
         Vector3 currentDir = carTransform.forward;
         Vector3 rightDir = Quaternion.Euler(0, 90, 0) * currentDir;
         Vector3 leftDir = Quaternion.Euler(0, -90, 0) * currentDir;
 
         Vector3 nextDirection = currentDir;
-        float delay = straightDelay;
+        float targetDistance = straightDistance;
         int choice = Random.Range(0, 3);
 
         switch (choice)
         {
             case 0:
                 nextDirection = currentDir;
-                delay = straightDelay;
+                targetDistance = straightDistance;
                 break;
             case 1:
                 nextDirection = rightDir;
-                delay = rightTurnDelay;
+                targetDistance = rightTurnDistance;
                 break;
             case 2:
                 nextDirection = leftDir;
-                delay = leftTurnDelay;
+                targetDistance = leftTurnDistance;
                 break;
         }
 
-        yield return new WaitForSeconds(delay);
+        Vector3 startPosition = carTransform.position;
+
+        while (car != null && Vector3.Distance(startPosition, carTransform.position) < targetDistance)
+        {
+            yield return null;
+        }
 
         if (car != null)
         {
