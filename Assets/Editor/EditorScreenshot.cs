d22a91fa07fc2b8ab36a7aa00ce0104d5dba993e
tzug1729 <195055595+tzug1729@users.noreply.github.com>
2026-09-20 16:50:11 +0900

    pキーをスクショに割りあてた

--- Assets/Editor/EditorScreenshot.cs
+++ Assets/Editor/EditorScreenshot.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 
 public static class EditorScreenshot
 {
-  [MenuItem("Tools/Screenshot")]
+  [MenuItem("Tools/Screenshot _p")]
   private static void TakeScreenshot()
   {
     var now = DateTime.Now;
-    // マニュアル用の画像は、Assetsの外にまとめて保存する。
     var directory = Path.GetFullPath(Path.Combine(Application.dataPath, "..", "context", "screenshots"));
     Directory.CreateDirectory(directory);
     var savePath = Path.Combine(directory, $"Screenshot{now.ToFileTime()}.png");
